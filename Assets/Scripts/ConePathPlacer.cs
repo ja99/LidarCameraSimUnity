d2b4644b980a1ecd5d9c4a8e9a4e7b476cc13c9d
ja99 <janis.boersig@gmx.de>
2021-07-04 19:00:58 +0200

    Added a track tool with autofollow (based on Sebastian Lagues Path Creator)

---
+++ Assets/Scripts/ConePathPlacer.cs
@@ -0,0 +1,83 @@
+﻿using PathCreation;
+using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    [ExecuteInEditMode]
+    public class ConePathPlacer : PathSceneTool
+    {
+        public GameObject leftConeprefab;
+        public GameObject rightConeprefab;
+        public GameObject leftStartingConeprefab;
+        public GameObject rightStartingConeprefab;
+        public GameObject holder;
+        public float spacing = 3;
+        public float trackwidth = 3;
+
+        const float minSpacing = .1f;
+
+        private Vector3 riseCones = new Vector3(0, 0.157f, 0);
+
+        //Place Cones along track
+        void Generate()
+        {
+            if (pathCreator != null && leftConeprefab != null && rightConeprefab != null &&
+                leftStartingConeprefab != null && rightStartingConeprefab != null && holder != null)
+            {
+                DestroyObjects();
+                VertexPath path = pathCreator.path;
+                spacing = Mathf.Max(minSpacing, spacing);
+                float dst = 0;
+
+
+                while (dst < path.length)
+                {
+                    Vector3 point = path.GetPointAtDistance(dst);
+
+                    Vector3 nextpoint = path.GetPointAtDistance(dst + 1);
+                    Vector3 directionVector = nextpoint - point;
+
+                    //Calculate the directional vector for the cones
+                    Vector3 widthDistance = Vector3.Cross(directionVector, new Vector3(0, 1, 0)) * trackwidth / 2;
+
+
+                    //Place Starting Cones
+                    if (dst == 0)
+                    {
+                        Instantiate(leftStartingConeprefab, point - widthDistance, Quaternion.identity,
+                            holder.transform);
+                        Instantiate(rightStartingConeprefab, point + widthDistance, Quaternion.identity,
+                            holder.transform);
+                    }
+                    else
+                    {
+                        //place Cones left and right of the track
+                        Instantiate(leftConeprefab, point + widthDistance + riseCones, Quaternion.identity,
+                            holder.transform);
+                        Instantiate(rightConeprefab, point - widthDistance + riseCones, Quaternion.identity,
+                            holder.transform);
+                    }
+
+                    dst += spacing;
+                }
+            }
+        }
+
+        void DestroyObjects()
+        {
+            int numChildren = holder.transform.childCount;
+            for (int i = numChildren - 1; i >= 0; i--)
+            {
+                DestroyImmediate(holder.transform.GetChild(i).gameObject, false);
+            }
+        }
+
+        protected override void PathUpdated()
+        {
+            if (pathCreator != null)
+            {
+                Generate();
+            }
+        }
+    }
+}
